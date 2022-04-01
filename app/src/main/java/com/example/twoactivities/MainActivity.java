e8f8b72004725083184d31d5507de7204910cded
prabidhipy <50479249+prabidhipy@users.noreply.github.com>
2022-04-01 22:36:48 +0545

    value restored when orientation changed

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.twoactivities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int count;
+    private TextView showCount;
+    private Button countIncrease;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        count = 0;
+        showCount = (TextView) findViewById(R.id.counter);
+        countIncrease = findViewById(R.id.count_button);
+
+        countIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                if(showCount != null){
+                    showCount.setText(Integer.toString(count));
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("countValue", count);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        count = savedInstanceState.getInt("countValue", 0);
+        showCount.setText(""+count);
     }
 }
