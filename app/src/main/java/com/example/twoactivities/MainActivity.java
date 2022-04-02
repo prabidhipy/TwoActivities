be38cf68eb34fe2b310b845d6abfd268db8f178f
prabidhipy <50479249+prabidhipy@users.noreply.github.com>
2022-04-02 19:23:55 +0545

    completed challenge

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -1,23 +1,77 @@
 package com.example.twoactivities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.TextViewOnReceiveContentListener;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.ServiceConfigurationError;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST = 1;
+    public static final String EXTRA_ADD = "com.example.twoactivities.extra.ADD";
+    ArrayList <String> itemList = new ArrayList<>(10);
+    TextView[] addedItems = new TextView[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addedItems[0] = findViewById(R.id.textView);
+        addedItems[1] = findViewById(R.id.textView2);
+        addedItems[2] = findViewById(R.id.textView3);
+        addedItems[3] = findViewById(R.id.textView4);
+        addedItems[4] = findViewById(R.id.textView5);
+        addedItems[5] = findViewById(R.id.textView6);
+        addedItems[6] = findViewById(R.id.textView7);
+        addedItems[7] = findViewById(R.id.textView8);
+        addedItems[8] = findViewById(R.id.textView9);
+        addedItems[9] = findViewById(R.id.textView10);
+
+        if(savedInstanceState != null){
+            itemList = savedInstanceState.getStringArrayList("itemsList");
+            if(itemList != null && itemList.size()>0){
+                for(int j=0; j<itemList.size(); j++){
+                    addedItems[j].setVisibility(View.VISIBLE);
+                    addedItems[j].setText(itemList.get(j));
+                }
+            }
+        }
     }
 
     public void launchSecondActivity(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, REQUEST);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(itemList.size() != 0){
+            outState.putStringArrayList("itemsList", itemList);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST){
+            if(resultCode == RESULT_OK){
+                String item = data.getStringExtra(SecondActivity.EXTRA_TEXT);
+                itemList.add(item);
+
+                for(int i = 0; i < itemList.size(); i++){
+                   addedItems[i].setVisibility(View.VISIBLE);
+                   addedItems[i].setText(itemList.get(i));
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -8,13 +8,59 @@ import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
 
+    public static final String EXTRA_TEXT = "com.example.twoactivities.extra.TEXT";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
     }
 
     public void launchFirstActivity(View view) {
+        Intent addItemIntent = new Intent();
 
+        switch (view.getId()){
+            case R.id.item1:
+                addItemIntent.putExtra(EXTRA_TEXT, "Milk");
+                setResult(RESULT_OK, addItemIntent);
+                finish();
+            case R.id.item2:
+                addItemIntent.putExtra(EXTRA_TEXT, "Toothpaste");
+                setResult(RESULT_OK, addItemIntent);
+                finish();
+            case R.id.item3:
+                addItemIntent.putExtra(EXTRA_TEXT, "Rice");
+                setResult(RESULT_OK, addItemIntent);
+                finish();
+            case R.id.item4:
+                addItemIntent.putExtra(EXTRA_TEXT, "Tissue");
+                setResult(RESULT_OK, addItemIntent);
+                finish();
+            case R.id.item5:
+                addItemIntent.putExtra(EXTRA_TEXT, "Tea");
+                setResult(RESULT_OK, addItemIntent);
+                finish();
+            case R.id.item6:
+                addItemIntent.putExtra(EXTRA_TEXT, "Fruits");
+                setResult(RESULT_OK, addItemIntent);
+                finish();
+            case R.id.item7:
+                addItemIntent.putExtra(EXTRA_TEXT, "Scissors");
+                setResult(RESULT_OK, addItemIntent);
+                finish();
+            case R.id.item8:
+                addItemIntent.putExtra(EXTRA_TEXT, "Cheese");
+                setResult(RESULT_OK, addItemIntent);
+                finish();
+            case R.id.item9:
+                addItemIntent.putExtra(EXTRA_TEXT, "Pens");
+                setResult(RESULT_OK, addItemIntent);
+                finish();
+            case R.id.item10:
+                addItemIntent.putExtra(EXTRA_TEXT, "Vegetables");
+                setResult(RESULT_OK, addItemIntent);
+                finish();
+        }
     }
 }
